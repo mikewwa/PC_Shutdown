3a5b85f1c2671c4dc589daa5c9350458b0daafdb
Michał Moraszczyk <michal.moraszczyk@aplusv.pl>
2026-03-17 23:48:05 +0100

    state machine approach

--- Program.cs
+++ Program.cs
@@ -1,77 +1,124 @@
 ﻿using System;
-using System.IO;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Management;
 using System.Net;
 using System.Net.Sockets;
-using System.Management;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
 
 namespace csharp_pc_shutdown_tcp_server_
 {
     internal class Program
     {
         private static string logPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.txt");
+        private static TcpListener server;
+        private const int PORT = 56789;
+        private static bool isLogEnabled = false;
 
         static void Main(string[] args)
         {
-            TcpListener server = null;
-            Log("Application started...");
+            // instantiate tcp server
+            server = new TcpListener(IPAddress.Any, PORT);
+            server.Start();
 
-            try
+            Console.WriteLine($"Server started listening on port {PORT}.");
+            if (isLogEnabled)
             {
-                //instantiate tcp server
-                Int32 port = 56789;
-                server = new TcpListener(IPAddress.Any, port);
+                Log($"Server started listening on port {PORT}.");
+            }
 
-                //start listening
-                server.Start();
+            // blocking call because Main() method is synchronous
+            Task listener = HandleListenerAsync(server);
+            listener.GetAwaiter().GetResult();
+        }
 
-                //buffer for reading data
-                byte[] bytes = new byte[256];
-
-                //enter the listening loop
-                while (true)
+        private static async Task HandleListenerAsync(TcpListener server)
+        {
+            // server is in the listening state
+            while (true)
+            {
+                // If a client is connected, a new socket is created, which is in the accepted state and can exchange information 
+                using (var client = await server.AcceptTcpClientAsync())
                 {
-                    Log("Waiting for connection...");
-                    using(TcpClient client = server.AcceptTcpClient())
+                    Console.WriteLine($"Connected to client: {client.Client.RemoteEndPoint}");
+                    if (isLogEnabled)
                     {
                         Log($"Connected to client: {client.Client.RemoteEndPoint}");
-                        NetworkStream stream = client.GetStream();
-                        int i;
-
-                        while((i = stream.Read(bytes, 0, bytes.Length)) != 0)
-                        {
-                            string data = Encoding.UTF8.GetString(bytes, 0, i).Trim().ToLowerInvariant();
-                            Log($"Received command: {data}");
-
-                            if (data.StartsWith("shutdown"))
-                            {
-                                bool success = ExecuteWmiAction(1, "Shutdown");
-                                SendResponse(stream, success ? "Starting_shutdown_fb\x0A" : "Error_shutdown_fb\x0A");
-                            }
-                            else if (data.StartsWith("reboot"))
-                            {
-                                bool success = ExecuteWmiAction(2, "Reboot");
-                                SendResponse(stream, success ? "Starting_reboot_fb\x0A" : "Error_reboot_fb\x0A");
-                            }
-                            else if (data.StartsWith("keep_alive"))
-                            {
-                                SendResponse(stream, "Keep_alive_fb\x0A");
-                            }
-                        }
                     }
+                    await HandleClientAsync(client);
+                }
+                    
+            }
+        }
+
+        private static async Task HandleClientAsync(TcpClient client)
+        {
+            //Send and receive information from client [bytes]
+            NetworkStream stream = client.GetStream();
+            Timer timer = new Timer(10000);
+            timer.AutoReset = false;
+            timer.Elapsed += (sender, args) => KeepAliveElapsed(sender, client);
+
+            string message;
+
+            while (true)
+            {
+                try
+                {
+                    timer.Start();
+                    byte[] buffer = new byte[1024];
+                    await stream.ReadAsync(buffer, 0, buffer.Length);
+
+                    message = Encoding.UTF8.GetString(buffer);
+                    HandleIncomingMessage(stream, message);
+                    timer.Stop();
+                }
+                catch(ObjectDisposedException)
+                {
+                    break;
                 }
             }
-            catch(Exception e)
+
+            Console.WriteLine("Client disconnected");
+        }
+
+        private static void KeepAliveElapsed(object sender, TcpClient client)
+        {
+            Console.WriteLine($"Keep alive timer for {client.Client.RemoteEndPoint} elapsed");
+            client.Close();
+        }
+
+        private static void HandleIncomingMessage(NetworkStream stream, string message)
+        {
+            if (message.ToLower().StartsWith("shutdown"))
             {
-                Log($"CRITICAL SERVER ERROR: {e}");
+                //bool success = ExecuteWmiAction(1, "Shutdown");
+                bool success = true;
+                SendResponse(stream, success ? "Starting_shutdown_fb\x0A" : "Error_shutdown_fb\x0A");
             }
-            finally
+            else if (message.ToLower().StartsWith("reboot"))
             {
-                server?.Stop();
-                Log("Server stopped.");
+                //bool success = ExecuteWmiAction(2, "Reboot");
+                bool success = true;
+                SendResponse(stream, success ? "Starting_reboot_fb\x0A" : "Error_reboot_fb\x0A");
+            }
+            else if (message.ToLower().StartsWith("keep_alive"))
+            {
+                SendResponse(stream, "Keep_alive_fb\x0A");
+            }
+            else if (message.ToLower().StartsWith("log_enable"))
+            {
+                isLogEnabled = true;
+                SendResponse(stream, "Log enabled.\x0A");
+            }
+            else if (message.ToLower().StartsWith("log_disable"))
+            {
+                isLogEnabled = false;
+                SendResponse(stream, "Log disabled.\x0A");
             }
         }
 
@@ -79,7 +126,10 @@ namespace csharp_pc_shutdown_tcp_server_
         {
             try
             {
-                Log($"Attempting {actionName} via WMI...");
+                if (isLogEnabled)
+                {
+                    Log($"Attempting {actionName} via WMI...");
+                }
                 ConnectionOptions op = new ConnectionOptions { EnablePrivileges = true };
                 ManagementScope scope = new ManagementScope(@"\\.\root\cimv2", op);
                 scope.Connect();
@@ -93,12 +143,18 @@ namespace csharp_pc_shutdown_tcp_server_
                     obj.InvokeMethod("Win32Shutdown", new object[] { flag, 0 });
                 }
 
-                Log($"{actionName} command sent successfully.");
+                if (isLogEnabled)
+                {
+                    Log($"{actionName} command sent successfully.");
+                }
                 return true;
             }
             catch (Exception e)
             {
-                Log($"WMI ERROR during {actionName}: {e.Message}\n{e.StackTrace}");
+                if (isLogEnabled)
+                {
+                    Log($"WMI ERROR during {actionName}: {e.Message}\n{e.StackTrace}");
+                }
                 return false;
             }
         }
